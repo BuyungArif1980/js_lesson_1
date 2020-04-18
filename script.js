54955e044ada5745da64e3d202df6e6c5c188746
yungbow <buyungarif.budpar@gmail.com>
2020-04-18 18:15:54 +0700

    Lesson 3. Kalkulasi (1)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Cetak "Hello World" ke console
-console.log("Hello World");
+// Cetak hasil 5 tambah 3
+console.log(5 + 3);
 
-// Cetak "Ninja Ken" ke console
-console.log("Ninja Ken");
+// Cetak hasil 20 kurang 8
+console.log(20 - 8);
 
-// Ubah baris dibawah menjadi komentar
-// console.log("Ubah baris ini menjadi komentar");
+// Cetak string "4 + 5"
+console.log("4 + 5");
