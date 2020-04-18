4980465504a55e1545c1eeca4b3c30ef383029b7
yungbow <buyungarif.budpar@gmail.com>
2020-04-18 20:30:59 +0700

    Lesson 13. Operator Boolean & Perbandingan (1)

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const level = 12;
+const age = 24;
 
-// Tambahkan pernyataan if dengan kondisi: level > 10
-if (level > 10) {
-  console.log("Level Anda lebih tinggi dari 10");
+// Cetak hasil dari age >= 18
+console.log(age >= 18);
+
+// Cetak hasil dari age < 18
+console.log(age < 18);
+
+// Ketika nilai dari age lebih besar atau sama dengan 18, cetak "Saya berusia lebih dari 18 tahun"
+if (age >= 18) {
+  console.log("Saya berusia lebih dari 18 tahun");
 }
